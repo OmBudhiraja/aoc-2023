fed530816e8ab5095beb18007f17af5e074f0f3a
Om Budhiraja <ombudhiraja144@gmail.com>
2023-12-03 21:44:34 +0530

    day 3 part 2 (messy but works)

--- 03/main.go
+++ 03/main.go
@@ -4,9 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"unicode"
 
 	"github.com/ombudhiraja/aoc-2023/utils"
 )
+
+type Point struct {
+	i int
+	j int
+}
 
 func main() {
 	lines, part := utils.Setup()
@@ -25,7 +31,7 @@ func part1(lines []string) {
 
 		for charIndex := 0; charIndex < len(line); charIndex++ {
 			if _, err := strconv.Atoi(string(line[charIndex])); err == nil {
-				partNumber, charIndexEnd, err := getPartNumber(line, lines, lineIndex, charIndex)
+				partNumber, charIndexEnd, _, err := getPartNumber(line, lines, lineIndex, charIndex)
 				charIndex += charIndexEnd
 				if err == nil {
 					sumOfPartNumber += partNumber
@@ -39,11 +45,54 @@ func part1(lines []string) {
 	fmt.Println("Part 1: ", sumOfPartNumber)
 }
 
-func part2(lines []string) {}
+func part2(lines []string) {
 
-func getPartNumber(line string, lines []string, lineIndex, charIndex int) (int, int, error) {
+	sumOfGearRatios := 0
+	gearMap := make(map[Point][]int)
+	var gearPos *Point
+	partNumber := 0
+
+	for lineIndex, line := range lines {
+		for charIndex := 0; charIndex < len(line); charIndex++ {
+
+			if unicode.IsDigit(rune(line[charIndex])) {
+				nr, charIndexEnd, p, err := getPartNumber(line, lines, lineIndex, charIndex)
+				charIndex += charIndexEnd
+
+				if err == nil {
+					gearPos = p
+					partNumber = nr
+				}
+			} else {
+				if gearPos != nil {
+					if _, ok := gearMap[*gearPos]; !ok {
+						gearMap[*gearPos] = make([]int, 0)
+					}
+
+					gearMap[*gearPos] = append(gearMap[*gearPos], partNumber)
+				}
+
+				gearPos = nil
+				partNumber = 0
+			}
+
+		}
+
+	}
+
+	for _, gear := range gearMap {
+		if len(gear) == 2 {
+			sumOfGearRatios += gear[0] * gear[1]
+		}
+	}
+
+	fmt.Println("Part 2: ", sumOfGearRatios)
+}
+
+func getPartNumber(line string, lines []string, lineIndex, charIndex int) (int, int, *Point, error) {
 
 	partNumber := ""
+	var gearPos *Point
 
 	for i := charIndex; i < len(line); i++ {
 		_, err := strconv.Atoi(string(line[i]))
@@ -55,19 +104,15 @@ func getPartNumber(line string, lines []string, lineIndex, charIndex int) (int, 
 
 	isPartNumberValid := false
 
+	neighbours := make([]Point, 0)
+
 	// check for any adjacent symbol
-
 	if charIndex > 0 { // left
-		if IsSymbol(string(line[charIndex-1])) {
-			isPartNumberValid = true
-		}
-
+		neighbours = append(neighbours, Point{lineIndex, charIndex - 1})
 	}
 
 	if charIndex+len(partNumber) < len(line) { // right
-		if IsSymbol(string(line[charIndex+len(partNumber)])) {
-			isPartNumberValid = true
-		}
+		neighbours = append(neighbours, Point{lineIndex, charIndex + len(partNumber)})
 	}
 
 	if lineIndex > 0 { // upper line
@@ -83,11 +128,7 @@ func getPartNumber(line string, lines []string, lineIndex, charIndex int) (int, 
 		}
 
 		for i := start; i < end; i++ {
-
-			if IsSymbol(string(lines[lineIndex-1][i])) {
-				isPartNumberValid = true
-				break
-			}
+			neighbours = append(neighbours, Point{i: lineIndex - 1, j: i})
 		}
 	}
 
@@ -104,29 +145,36 @@ func getPartNumber(line string, lines []string, lineIndex, charIndex int) (int, 
 		}
 
 		for i := start; i < end; i++ {
+			neighbours = append(neighbours, Point{i: lineIndex + 1, j: i})
+		}
+	}
 
-			if IsSymbol(string(lines[lineIndex+1][i])) {
-				isPartNumberValid = true
-				break
+	for _, neighbour := range neighbours {
+		char := lines[neighbour.i][neighbour.j]
+
+		if IsSymbol(rune(char)) {
+			isPartNumberValid = true
+			if string(char) == "*" {
+				gearPos = &Point{
+					i: neighbour.i,
+					j: neighbour.j,
+				}
 			}
+			break
 		}
 	}
 
 	if isPartNumberValid {
 		partNr, _ := strconv.Atoi(partNumber)
 
-		return partNr, len(partNumber), nil
+		return partNr, len(partNumber) - 1, gearPos, nil
 	}
 
-	return 0, len(partNumber), errors.New("invalid part number")
+	return 0, len(partNumber) - 1, gearPos, errors.New("invalid part number")
 }
 
-func IsSymbol(s string) bool {
-	if s == "." {
-		return false
-	}
-
-	if _, err := strconv.Atoi(s); err == nil {
+func IsSymbol(s rune) bool {
+	if s == '.' || unicode.IsDigit((s)) {
 		return false
 	}
 
