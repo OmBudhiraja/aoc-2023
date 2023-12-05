78cd890786322c3404ca67427a9c90dfd34be44e
Om Budhiraja <ombudhiraja144@gmail.com>
2023-12-06 01:36:49 +0530

    day5 #2 (seems right but doesn't work for the required input)

--- 05/main.go
+++ 05/main.go
@@ -13,6 +13,7 @@ func main() {
 	lines := utils.Setup()
 
 	part1Result := math.MaxInt64
+	part2Result := math.MaxInt64
 
 	seeds := utils.Mapper(strings.Split(strings.TrimSpace(strings.Split(lines[0], ":")[1]), " "), func(s string) int {
 		n, err := strconv.Atoi(s)
@@ -20,14 +21,16 @@ func main() {
 		return n
 	})
 
-	conversions := seeds
+	conversions := make([]int, 0)
+
+	conversions = append(conversions, seeds...)
 
 	groupedLines := strings.Split(strings.Join(lines[2:], "\n"), "\n\n")
 
-	for _, conversionMap := range groupedLines {
-		lines := strings.Split(conversionMap, "\n")
+	for _, data := range groupedLines {
+		lines := strings.Split(data, "\n")
 
-		for seedIndex, seed := range seeds {
+		for seedIndex := range seeds {
 			for lineIndex, line := range lines {
 				if lineIndex == 0 {
 					continue
@@ -37,15 +40,53 @@ func main() {
 				sourceRangeStart, _ := strconv.Atoi(conversionData[1])
 				rangeLength, _ := strconv.Atoi(conversionData[2])
 
-				if sourceRangeStart <= seed && seed <= sourceRangeStart+rangeLength-1 {
-					// found = true
-					conversions[seedIndex] = seed - sourceRangeStart + destinationRangeStart
+				if sourceRangeStart <= conversions[seedIndex] && conversions[seedIndex] <= sourceRangeStart+rangeLength-1 {
+					conversions[seedIndex] = conversions[seedIndex] - sourceRangeStart + destinationRangeStart
+					break
 				} else {
 					continue
 				}
 			}
 		}
 
+	}
+
+	for i, seed := range seeds {
+		if i%2 != 0 {
+			continue
+		}
+
+		for j := seed; j < seed+seeds[i+1]; j++ {
+
+			finalConvertedValue := j
+
+			for _, data := range groupedLines {
+				lines := strings.Split(data, "\n")
+
+				for lineIndex, line := range lines {
+					if lineIndex == 0 {
+						continue
+					}
+
+					conversionData := strings.Split(strings.TrimSpace(line), " ")
+					destinationRangeStart, _ := strconv.Atoi(conversionData[0])
+					sourceRangeStart, _ := strconv.Atoi(conversionData[1])
+					rangeLength, _ := strconv.Atoi(conversionData[2])
+
+					if sourceRangeStart <= finalConvertedValue && finalConvertedValue <= sourceRangeStart+rangeLength-1 {
+						finalConvertedValue = finalConvertedValue - sourceRangeStart + destinationRangeStart
+						break
+					} else {
+						continue
+					}
+				}
+
+			}
+
+			if finalConvertedValue < part2Result {
+				part2Result = finalConvertedValue
+			}
+		}
 	}
 
 	for _, conversion := range conversions {
@@ -55,5 +96,6 @@ func main() {
 	}
 
 	fmt.Println("Part 1:", part1Result)
+	fmt.Println("Part 2:", part2Result)
 
 }
