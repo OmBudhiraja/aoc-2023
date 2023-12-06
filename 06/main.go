eb7572ded9246787aa653debe2967a2db86f2199
Om Budhiraja <ombudhiraja144@gmail.com>
2023-12-06 21:44:32 +0530

    day 6 part 2 done

--- 06/main.go
+++ 06/main.go
@@ -12,9 +12,13 @@ func main() {
 	lines := utils.Setup()
 
 	part1Result := 1
+	part2Result := 0
 
 	raceTimes := filterEmptySpaces(strings.Split(strings.TrimSpace(strings.Split(lines[0], ":")[1]), " "))
 	distanceRecords := filterEmptySpaces(strings.Split(strings.TrimSpace(strings.Split(lines[1], ":")[1]), " "))
+
+	joinedRaceTime, _ := strconv.Atoi(strings.Join(raceTimes, ""))
+	joinedDistanceRecord, _ := strconv.Atoi(strings.Join(distanceRecords, ""))
 
 	for i := 0; i < len(raceTimes); i++ {
 		time, _ := strconv.Atoi(raceTimes[i])
@@ -32,7 +36,15 @@ func main() {
 		part1Result *= nrOfWaysToWin
 	}
 
+	for hold := 1; hold < joinedRaceTime; hold++ {
+		distanceCovered := hold * (joinedRaceTime - hold)
+		if distanceCovered > joinedDistanceRecord {
+			part2Result++
+		}
+	}
+
 	fmt.Println("Part 1:", part1Result)
+	fmt.Println("Part 2:", part2Result)
 }
 
 func filterEmptySpaces(str []string) []string {
